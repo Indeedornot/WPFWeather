60da72f77f783376e4a586b82bdebb6adaab6089
indeedornot <gravitybartek@gmail.com>
2023-01-19 22:03:39 +0100

    ref: Use Multibinding for visibility in WeatherHomeView
    WIP: Scroll buttons for listview

---
+++ WPFWeather/Converters/InvertBooleanConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WPFWeather.Converters;
+public class InvertBooleanConverter : IValueConverter {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+        if (value is bool bValue) {
+            return !bValue;
+        }
+
+        throw new ArgumentException("Value is not a boolean", nameof(value));
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+        if (value is bool bValue) {
+            return !bValue;
+        }
+
+        throw new ArgumentException("Value is not a boolean", nameof(value));
+    }
+}
--- WPFWeather/Views/WeatherHomeView.xaml.cs
+++ WPFWeather/Views/WeatherHomeView.xaml.cs
@@ -1,11 +1,17 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace WPFWeather.Views;
 /// <summary>
 /// Interaction logic for WeatherHome.xaml
 /// </summary>
 public partial class WeatherHomeView : UserControl {
+    public string Message { get; } = "Hi";
     public WeatherHomeView() {
         InitializeComponent();
+    }
+
+    public void SayHello(object sender, RoutedEventArgs e) {
+        MessageBox.Show(Message);
     }
 }
