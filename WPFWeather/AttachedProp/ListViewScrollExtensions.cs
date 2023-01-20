7c3c6110eb102c072ad32747504cc921d6c83cef
indeedornot <gravitybartek@gmail.com>
2023-01-20 15:30:11 +0100

    WIP: AttachedProperty scroll events for ListView

---
+++ WPFWeather/AttachedProp/ListViewScrollExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WPFWeather.AttachedProp;
+
+//https://stackoverflow.com/a/34284262/11262883
+public class ListViewScrollExtensions {
+    public static readonly DependencyProperty ScrollChangedCommandProperty =
+        DependencyProperty.RegisterAttached(
+            "ScrollChangedCommand",
+            typeof(ICommand),
+            typeof(ListViewScrollExtensions),
+            new PropertyMetadata(default(ICommand),
+            OnScrollChangedCommandChanged));
+
+    private static void OnScrollChangedCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+        ListView? listView = d as ListView;
+        if (listView == null) return;
+
+        if (e.NewValue != null) {
+            listView.Loaded += DataGridOnLoaded;
+            return;
+        }
+
+        if (e.OldValue != null) {
+            listView.Loaded -= DataGridOnLoaded;
+            return;
+        }
+    }
+
+    private static void DataGridOnLoaded(object sender, RoutedEventArgs routedEventArgs) {
+        ListView? listView = sender as ListView;
+        if (listView == null) return;
+
+        ScrollViewer? scrollViewer = UIHelper.FindChildren<ScrollViewer>(listView).FirstOrDefault();
+        if (scrollViewer == null) return;
+
+        scrollViewer.ScrollChanged += ScrollViewerOnScrollChanged;
+    }
+
+    private static void ScrollViewerOnScrollChanged(object sender, ScrollChangedEventArgs e) {
+        var scrollViewer = sender as ScrollViewer;
+        if (scrollViewer == null) return;
+
+        ListView? listView = UIHelper.FindParent<ListView>(scrollViewer);
+        if (listView == null) return;
+
+        ICommand command = GetScrollChangedCommand(listView);
+        command.Execute(e);
+    }
+
+    public static void SetScrollChangedCommand(DependencyObject element, ICommand value) {
+        element.SetValue(ScrollChangedCommandProperty, value);
+    }
+
+    public static ICommand GetScrollChangedCommand(DependencyObject element) {
+        return (ICommand)element.GetValue(ScrollChangedCommandProperty);
+    }
+}
---
+++ WPFWeather/AttachedProp/UIHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace WPFWeather.AttachedProp;
+internal static class UIHelper {
+    internal static IList<T> FindChildren<T>(DependencyObject element) where T : FrameworkElement {
+        List<T> retval = new();
+
+        for (int counter = 0; counter < VisualTreeHelper.GetChildrenCount(element); counter++) {
+            FrameworkElement? toadd = VisualTreeHelper.GetChild(element, counter) as FrameworkElement;
+            if (toadd == null) continue;
+
+            if (toadd is T correctlyTyped) {
+                retval.Add(correctlyTyped);
+            }
+            else {
+                retval.AddRange(FindChildren<T>(toadd));
+            }
+        }
+
+        return retval;
+    }
+
+    internal static T? FindParent<T>(DependencyObject element) where T : FrameworkElement {
+        FrameworkElement? parent = VisualTreeHelper.GetParent(element) as FrameworkElement;
+        if (parent == null) return null;
+
+        if (parent is T correctlyTyped) {
+            return correctlyTyped;
+        }
+
+        return FindParent<T>(parent);
+    }
+}
