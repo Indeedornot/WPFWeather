3c4048129dc6530d602fbe5273d60c7127b70dde
indeedornot <gravitybartek@gmail.com>
2023-01-20 16:17:19 +0100

    WIP: HitMax and HitMin Events for ScrollExtensions

--- WPFWeather/AttachedProp/ListViewScrollExtensions.cs
+++ WPFWeather/AttachedProp/ListViewScrollExtensions.cs
@@ -6,31 +6,58 @@ using System.Windows.Input;
 namespace WPFWeather.AttachedProp;
 
 //https://stackoverflow.com/a/34284262/11262883
+//Bug: Double registers events
 public class ListViewScrollExtensions {
-    public static readonly DependencyProperty ScrollChangedCommandProperty =
+    #region ScrollHitMaxCommand
+    public static readonly DependencyProperty ScrollHitMaxCommandProperty =
         DependencyProperty.RegisterAttached(
-            "ScrollChangedCommand",
+            "ScrollHitMaxCommand",
             typeof(ICommand),
             typeof(ListViewScrollExtensions),
             new PropertyMetadata(default(ICommand),
             OnScrollChangedCommandChanged));
+
+    public static void SetScrollHitMaxCommand(DependencyObject element, ICommand value) {
+        element.SetValue(ScrollHitMaxCommandProperty, value);
+    }
+    public static ICommand GetScrollHitMaxCommand(DependencyObject element) {
+        return (ICommand)element.GetValue(ScrollHitMaxCommandProperty);
+    }
+    #endregion
+    #region ScrollHitMinCommand
+    public static readonly DependencyProperty ScrollHitMinCommandProperty =
+        DependencyProperty.RegisterAttached(
+            "ScrollHitMinCommand",
+            typeof(ICommand),
+            typeof(ListViewScrollExtensions),
+            new PropertyMetadata(default(ICommand),
+            OnScrollChangedCommandChanged));
+
+    public static void SetScrollHitMinCommand(DependencyObject element, ICommand value) {
+        element.SetValue(ScrollHitMinCommandProperty, value);
+    }
+    public static ICommand GetScrollHitMinCommand(DependencyObject element) {
+        return (ICommand)element.GetValue(ScrollHitMinCommandProperty);
+    }
+    #endregion
+
 
     private static void OnScrollChangedCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
         ListView? listView = d as ListView;
         if (listView == null) return;
 
         if (e.NewValue != null) {
-            listView.Loaded += DataGridOnLoaded;
+            listView.Loaded += ListViewOnLoaded;
             return;
         }
 
         if (e.OldValue != null) {
-            listView.Loaded -= DataGridOnLoaded;
+            listView.Loaded -= ListViewOnLoaded;
             return;
         }
     }
 
-    private static void DataGridOnLoaded(object sender, RoutedEventArgs routedEventArgs) {
+    private static void ListViewOnLoaded(object sender, RoutedEventArgs routedEventArgs) {
         ListView? listView = sender as ListView;
         if (listView == null) return;
 
@@ -47,15 +74,35 @@ public class ListViewScrollExtensions {
         ListView? listView = UIHelper.FindParent<ListView>(scrollViewer);
         if (listView == null) return;
 
-        ICommand command = GetScrollChangedCommand(listView);
-        command.Execute(e);
+        HandleScrollChanged(listView, e);
+        //command.Execute(e);
     }
 
-    public static void SetScrollChangedCommand(DependencyObject element, ICommand value) {
-        element.SetValue(ScrollChangedCommandProperty, value);
-    }
+    private static void HandleScrollChanged(ListView listView, ScrollChangedEventArgs e) {
+        ICommand MaxHitCommand = GetScrollHitMaxCommand(listView);
+        ICommand MinHitCommand = GetScrollHitMinCommand(listView);
 
-    public static ICommand GetScrollChangedCommand(DependencyObject element) {
-        return (ICommand)element.GetValue(ScrollChangedCommandProperty);
+        // && e.VerticalChange == 0
+        if (e.HorizontalChange == 0) return;
+
+        if (e.HorizontalOffset == 0) {
+            MinHitCommand.Execute(e);
+            return;
+        }
+
+        if (e.HorizontalOffset == e.ExtentWidth - e.ViewportWidth) {
+            MaxHitCommand.Execute(e);
+            return;
+        }
+
+
+        //if (e.VerticalChange != 0) {
+        //    if (e.VerticalOffset == 0) {
+        //        MinHitCommand.Execute(e);
+        //    }
+        //    else if (e.VerticalOffset == e.ViewportHeight - e.ExtentHeight) {
+        //        MaxHitCommand.Execute(e);
+        //    }
+        //}
     }
 }
--- WPFWeather/Views/WeatherHomeView.xaml.cs
+++ WPFWeather/Views/WeatherHomeView.xaml.cs
@@ -1,17 +1,34 @@
-﻿using System.Windows;
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+
+using WPFWeather.Commands;
+using WPFWeather.ViewModels;
 
 namespace WPFWeather.Views;
+
 /// <summary>
 /// Interaction logic for WeatherHome.xaml
 /// </summary>
 public partial class WeatherHomeView : UserControl {
-    public string Message { get; } = "Hi";
+    public RelayCommand HitMaxCommand { get; }
+    public RelayCommand HitMinCommand { get; }
+
     public WeatherHomeView() {
         InitializeComponent();
+
+        HitMaxCommand = new(HitMax);
+        HitMinCommand = new(HitMin);
     }
 
-    public void SayHello(object sender, RoutedEventArgs e) {
-        MessageBox.Show(Message);
+    private void HitMax(object parameter) {
+        Debug.WriteLine("HitMax");
+    }
+
+    private void HitMin(object parameter) {
+        Debug.WriteLine("HitMin");
     }
 }
